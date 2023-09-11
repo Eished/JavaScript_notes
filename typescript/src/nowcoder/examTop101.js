b0c29b810fe77d36c0d3e85abc9bb95b4eb434a1
Eished <kished@outlook.com>
2023-09-11 10:55:55 +0800

    增加二分查找

--- typescript/src/nowcoder/examTop101.js
+++ typescript/src/nowcoder/examTop101.js
@@ -29,3 +29,37 @@ function BM1() {
   console.log(ReverseList(new ListNode(1)))
 }
 // BM1()
+
+function BM17() {
+  // 二分查找
+  const target = 13
+  const arr = [-1, 0, 3, 4, 6, 10, 13, 14]
+
+  const search = function (nums, target) {
+    // 在区间[left,right]中查找元素，左闭右闭
+    let left = 0
+    let right = nums.length - 1
+    while (left <= right) {
+      // 计算中间点
+      let mid = parseInt(left + (right - left) / 2)
+      if (target == nums[mid]) {
+        return mid
+        // 如果target < nums[mid]，表示目标值可能在左半边
+      } else if (target < nums[mid]) {
+        right = mid - 1
+        // 如果target > nums[mid]，表示目标值可能在右半边
+      } else if (target > nums[mid]) {
+        left = mid + 1
+      }
+    }
+    // 未找到返回-1
+    return -1
+  }
+
+  console.log(search(arr, target))
+}
+BM17()
+
+function BM23() {
+  console.log(1)
+}
